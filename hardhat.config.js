f2c7cbe075344b4a9a8159d3c5d3877b5da718a2
elanmustvision25-foomer <elanmust2025@gmail.com>
2026-03-05 14:46:05 +0100

    Update deployed addresses - new PIECore + GFLOIgnition

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,21 @@
-// Ideiglenes hardhat.config.js a teszteléshez
 import "@nomicfoundation/hardhat-ethers";
+import "@nomicfoundation/hardhat-verify";
+import dotenv from "dotenv";
+import { homedir } from "os";
+
+dotenv.config({ path: homedir() + "/.env" });
 
 export default {
   solidity: "0.8.28",
   networks: {
     sepolia: {
-      url: "https://ethereum-sepolia-rpc.publicnode.com",
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: {
+        mnemonic: process.env.MNEMONIC
+      }
     }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
   }
 };
